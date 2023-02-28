7b806996f48d7fbbc9d49605f52acfa4bf5d4e20
Justin Seanez <jtseanez@ucdavis.edu>
2023-02-27 20:21:11 -0800

    Finished the Geese Filter function

--- array/geeseFilter/main.js
+++ array/geeseFilter/main.js
@@ -1,5 +1,5 @@
 function gooseFilter (birds) {
     var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-    
+    return birds.filter(b => !geese.includes(b));
     // return an array containing all of the strings in the input array except those that match strings in geese
 };
