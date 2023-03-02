06599540beb2c480e5a1dd9e22f20147a475c9db
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-01 16:33:31 -0800

    Finished BMI calc problem

--- 8kyu/multiplication/BMICalc/main.js
+++ 8kyu/multiplication/BMICalc/main.js
@@ -1,3 +1,15 @@
 function bmi(weight, height) {
-    return "";
+    let BMI = weight/Math.pow(height,2)
+    if (BMI <= 18.5){
+      return "Underweight"
+    }
+    if (BMI <= 25){
+      return "Normal"
+    }
+    if (BMI <= 30){
+      return "Overweight"
+    }
+    if (BMI > 30){
+      return "Obese"
+    }
 }
