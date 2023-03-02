641f7f42bbbc95635e33d1f03e137b29d027d355
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-01 16:26:51 -0800

    Completed Fake Binary problem

--- 8kyu/strings/fakebinary/main.js
+++ 8kyu/strings/fakebinary/main.js
@@ -1,3 +1,11 @@
 function fakeBin(x){
-
+    let result = "";
+    for (let i = 0; i < x.length; i++) {
+      if (parseInt(x[i]) < 5) {
+        result += "0";
+      } else {
+        result += "1";
+      }
+    }
+    return result;
 }
