fdfe7a67632a10763d037902fabaa30d44a22ff3
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-02 17:34:06 -0800

    Finished final 8kyu problem

--- codewarsStringLadder/8kyu/totalAmountOfPoints/main.js
+++ codewarsStringLadder/8kyu/totalAmountOfPoints/main.js
@@ -1,3 +1,12 @@
 function points(games) {
-    return 0
+    let points = 0;
+    for (let i = 0; i < 10; i++) {
+      const [x, y] = games[i].split(':').map(Number);
+      if (x > y) {
+        points += 3;
+      } else if (x === y) {
+        points += 1;
+      }
+    }
+    return points;
 }
