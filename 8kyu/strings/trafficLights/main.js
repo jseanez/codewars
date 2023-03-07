bd8eee07c3eba9fa2306321efe6d6f9989f07524
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-06 21:27:35 -0800

    solved traffic light logic problem

--- 8kyu/strings/trafficLights/main.js
+++ 8kyu/strings/trafficLights/main.js
@@ -1,5 +1,9 @@
 function updateLight(current) {
-  
-    //your code here!
-  
-}
+    if (current === "green"){
+      return "yellow";
+    } else if(current === "yellow"){
+      return "red";
+    }else{
+      return "green";
+    }
+  }
