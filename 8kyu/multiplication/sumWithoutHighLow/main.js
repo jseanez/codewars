199218f2f468a70cb7df815f93c9222b637d8abc
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-06 21:44:51 -0800

    solved the sum of the sorted array from least to greatest without the first or last array values

--- 8kyu/multiplication/sumWithoutHighLow/main.js
+++ 8kyu/multiplication/sumWithoutHighLow/main.js
@@ -1,3 +1,10 @@
 function sumArray(array) {
-
+    if (array === null || array.length === 0){
+      return 0
+    }else{
+    let sortedArr = array.sort((a, b) => a - b);
+    sortedArr.shift();
+    sortedArr.pop();
+    return sortedArr.reduce((acc, cv)=> acc + cv, 0)
+    }
 }
