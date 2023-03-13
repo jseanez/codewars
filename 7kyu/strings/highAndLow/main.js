7b2c25671056a416cf201a5f4a165908b64b3f9e
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-13 15:30:18 -0700

    Completed PREP for the highAndLow problem

---
+++ 7kyu/strings/highAndLow/main.js
@@ -0,0 +1,21 @@
+function highAndLow(numbers){
+  // ...
+}
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+// Parameters 
+// String of space separated numbers, numbers are whole numbers. Always atleast 1 number in the input string.
+
+// Return
+// Return must be 2 numbers separated by a single space with the highest number first. 
+
+
+// Examples
+// highAndLow("1 2 3 4 5") = "5 1"
+
+// Pseudo Code
+// In order to sort from least to greatest I need to turn the input string into an array using .split() after this I must sort the array from least to greatest using .sort(), after the array is sorted least to greatest I can store the least and greatest value of the array by storing them in a variables using .pop() and .shift(). Then combining those 2 variables into 2 numbers separated with a single space with the highest number first.
