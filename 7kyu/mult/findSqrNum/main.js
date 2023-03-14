0755eee109cd90e2ecaf4d4a6f86993aa660166e
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-14 12:15:29 -0700

    solved the findSqrNum problem

--- 7kyu/mult/findSqrNum/main.js
+++ 7kyu/mult/findSqrNum/main.js
@@ -16,3 +16,11 @@
 // Pseudo Code
 // First I want to check if n is a perfect square number. To do this I will use the static method of Math.sqrt() to get the square root of n. Then inside a conditional statement I will check if the sqrt if a whole number using the modulus (remainder operator) to see if the remainder of the Math.sqrt(n) is 0 by dividing it by 1 and checking the remainder. And inside this same conditional statement I will make sure that the Math.sqrt(n) is a posistive number using && inside the first if statement. Then I will return true and write the else to return false.
 
+var isSquare = function(n){
+    if (Math.sqrt(n) % 1 === 0 && Math.sqrt(n) >= 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
