85d8db1ef5eb58c0a416bc0da95e9849395f582c
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-16 21:03:43 -0700

    Added PREP for isogram problem

---
+++ 7kyu/strings/isograms/main.js
@@ -0,0 +1,28 @@
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+// isIsogram "Dermatoglyphics" = true
+// isIsogram "moose" = false
+// isIsogram "aba" = false
+// function isIsogram(str){
+    
+//     }
+// Parameters
+// String with only letters, assume empty strings are isograms.
+
+// Return
+// True if string is an isogram, false the string isn't an isogram.
+
+// Example
+// isIsogram "Dermatoglyphics" = true
+// isIsogram "moose" = false
+// isIsogram "aba" = false
+
+// Pseudo Code
+// I want to sort all of the characters in the string into an array so I can loop through that array and check if any elements in that array are sitting next to the same element. This would mean that the word isn't an isogram if it contains multiple letters in the same word. 
+
+
+ 
