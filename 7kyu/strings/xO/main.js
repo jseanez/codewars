acc299119a2bf9ada9512a948d3f0ab063af894e
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-16 21:08:57 -0700

    Added PREP for x's and o's problem

---
+++ 7kyu/strings/xO/main.js
@@ -0,0 +1,29 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+// function XO(str) {
+//     //code here
+// }
+
+// Parameters
+// String with any character
+
+// Return 
+// true or false
+
+// Examples
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+
+// Pseudo Code
+// Turn the string into an array to use the .forEach() on the array to tally how many x and o are in the array then return true if num of x === num of o, and false if otherwise.
