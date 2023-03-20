200ca2120e21202e46fd7ce38c4979a567b2a9d3
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-19 22:59:32 -0700

    Added PREP for shortest Word problem

---
+++ 7kyu/strings/shortestWord/main.js
@@ -0,0 +1,18 @@
+// function findShort(s){
+// }
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+
+// Parameters
+// String of words that aren't empty
+
+// Return
+// The length of the shortest word among the strings that are given. I think it should be a number?
+
+// Example
+// ("words", "longerwords", "Hi") => 2
+
+// Pseudo Code
+// Put the length of each word into an array. Then sort the array using the .sort() method and returning the first number in the array and make sure to wrap Number() around it.
