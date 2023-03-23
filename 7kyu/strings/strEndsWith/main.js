d0c4070adb08337e18ac9eca396504675e4fe703
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-22 20:40:16 -0700

    Added PREP for string ends with problem

---
+++ 7kyu/strings/strEndsWith/main.js
@@ -0,0 +1,23 @@
+// function solution(str, ending){
+//     // TODO: complete
+//   }
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+// Parameters
+// 2 Strings, str and ending. 
+
+// Return
+// True if the first string ends with the 2nd string, false otherwise.
+
+// Example
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+// Pseudo Code
+// Return str.endsWith(ending) since it will return true if the first string ends with ending and false otherwise.
+
