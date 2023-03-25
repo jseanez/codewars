d2a9dd79d30a11ec3520bf41dcec90aae74e5ccc
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-24 21:39:17 -0700

    added PREP for create a phone number problem

--- 6kyu/array/createPhoneNumber/main.js
+++ 6kyu/array/createPhoneNumber/main.js
@@ -10,12 +10,16 @@
 // Don't forget the space after the closing parentheses!
 
 // Parameters
+// Array of 10 integers that are between 0 and 9. 
 
 
 // Return
+// A string of the numbers in the array in the format of a phone number. "(123) 456-789"
 
 
 // Example
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
 
 
 // Pseudo Code
+// Add ( to the 0 index of the numbers array and ) to the 4th index of the numbers array. Add " " to the 5th index of the numbers array. Add - to the 9th index of the array. Then return the array as string with .join('')
