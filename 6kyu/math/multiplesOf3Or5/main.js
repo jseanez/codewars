0a8364f3575fd5b4725361c7ab866d3691bae5c1
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-24 21:25:58 -0700

    completed PREP for mult of 3 or 5 problem

--- 6kyu/math/multiplesOf3Or5/main.js
+++ 6kyu/math/multiplesOf3Or5/main.js
@@ -10,12 +10,13 @@
 // Courtesy of projecteuler.net (Problem 1)
 
 // Parameters
-
+// Single integer number. 
 
 // Return
-
+// The sum of all of the numbers below the inputed number that are multiples of 3 and 5.
 
 // Example
-
+// function solution(10) => 3 + 5 + 6 + 9 = 23
 
 // Pseudo Code
+// I can use a for loop to loop through each number up to the input number. This will let me check each number below the input and see if it is a multiple of 3 or 5. I can create an empty array to store these numbers that are multiples of 3 or 5. Then I can use reduce to sum all of those numbers and return the answer
