5b2f7a35749a5816c8d806ce5dd16303b314d921
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-25 18:05:08 -0700

    solved correct string mistakes problem

---
+++ 8kyu/strings/correctMistakes/main.js
@@ -0,0 +1,31 @@
+// function correct(string)
+// {
+// 	// your code here
+// }
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+
+// Parameters
+// Strings that replaced S with 5, O with 0, and I with 1.
+
+
+// Return
+// The original string with any instance of 5,0,1 replaced with S,O,I
+
+
+
+// Example
+// correct("L0ND0N"),"LONDON"
+
+
+
+// Pseudo Code
+// Use replaceAll to return the string with all 3 characters replaced.
