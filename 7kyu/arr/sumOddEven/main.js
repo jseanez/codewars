72dcb0b4b74fd2067f0c9d5181909027d192cf86
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-28 19:45:33 -0700

    addded PREP for sumOddEven

---
+++ 7kyu/arr/sumOddEven/main.js
@@ -0,0 +1,48 @@
+// function oddOrEven(array) {
+//    //enter code here
+// }
+// Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+// Have fun!
+
+// Parameters
+// An array of integers, If the array is empty consider its value as 0.
+
+
+
+// Return
+// "even" if the sum of all the numbers in the array is even and "odd" if the sum of all the numbers in the array is odd.
+
+
+
+
+// Example
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+
+
+
+
+// Pseudo Code
+// First check if the arr is empty and return 'even' if that is the case. After checking for that I will use the reduce method to find the sum of the array and store it check if it is even or odd and return "even" or "odd using a ternary"
+
