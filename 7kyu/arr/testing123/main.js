7a7b7e8fcff3cb9a487edd6243bbf77f64d34dcb
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-29 20:48:45 -0700

    added prep for testing123

---
+++ 7kyu/arr/testing123/main.js
@@ -0,0 +1,35 @@
+// var number=function(array){
+//     //your awesome code here
+// }
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+// Parameters
+// A list of strings in an array, There can be empty arrays
+
+
+
+// Return
+// (Index plus 1: element) repeated in an array
+
+
+
+
+// Example
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+
+
+
+
+// Pseudo Code
+// Use a map() function to modify each element and index in the array and use template literals to return the correct array. 
