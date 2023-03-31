ae5cef7c097578298aab768ac6f3e03932f41fe2
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-30 21:45:27 -0700

    added prep for switch it up problem

---
+++ 8kyu/conversions/switchItUp/main.js
@@ -0,0 +1,14 @@
+// function switchItUp(number){
+// //Write your own Code!
+// }
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+// The input number will always be between 0 and 9 and be a single number. I want to return that number in written out form as a string.
+
+// The only way right now I can think of solving this is switch case or a long if else statement
