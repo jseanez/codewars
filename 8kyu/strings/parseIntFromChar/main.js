e340b364d9abf2d837382b5f698c51dfbb667fde
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-30 21:39:27 -0700

    Added PREP for parse Int From Char

---
+++ 8kyu/strings/parseIntFromChar/main.js
@@ -0,0 +1,15 @@
+// function getAge(inputString){
+//     // return the girl's correct age as an integer. Happy coding :) 
+//     }
+
+//     You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+//     Write a program that returns the girl's age (0-9) as an integer.
+    
+//     Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+    
+
+// So we have an input string that always has the format of age years old, and age is a random number between 0 and 9. I want to take that number out of the string and return it as an integer. The first character in the string is always a number.
+
+// First I will take the first character out of the string with inputString[0], then I will wrap it in Number() to ensure that it will be a number, then I will return it.
+
