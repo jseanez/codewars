7aed5f789968fb24daaa65d4e02c61657a568e63
Justin Seanez <jtseanez@ucdavis.edu>
2023-03-30 21:45:52 -0700

    solved switch it up problem

--- 8kyu/conversions/switchItUp/main.js
+++ 8kyu/conversions/switchItUp/main.js
@@ -12,3 +12,37 @@
 // The input number will always be between 0 and 9 and be a single number. I want to return that number in written out form as a string.
 
 // The only way right now I can think of solving this is switch case or a long if else statement
+function switchItUp(number){
+    switch(number) {
+        case 9:
+          return "Nine";
+          break;
+        case 8:
+          return "Eight";
+          break;
+        case 7:
+          return "Seven";
+          break;
+        case 6:
+          return "Six";
+          break;
+        case 5:
+          return "Five";
+          break;
+        case 4:
+          return "Four";
+          break;
+        case 3:
+          return "Three";
+          break;
+        case 2:
+          return "Two";
+          break;
+        case 1:
+          return "One";
+          break;
+        case 0:
+          return "Zero";
+          break;
+    }
+  }
